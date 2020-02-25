dbe9cb01fb827b1f92264a445e6bd762e1800e31
Souravrax <rakshitsourav3@gmail.com>
2020-02-26 01:55:28 +0530

    Removed redundency

--- js/main.js
+++ js/main.js
@@ -1,87 +1,119 @@
+$(function() {
 
-(function ($) {
-    "use strict";
-
-
-    /*==================================================================
-    [ Focus input ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
-                $(this).addClass('has-val');
-            }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })    
+    $(".input input").focus(function() {
+ 
+       $(this).parent(".input").each(function() {
+          $("label", this).css({
+             "line-height": "18px",
+             "font-size": "18px",
+             "font-weight": "100",
+             "top": "0px"
+          })
+          $(".spin", this).css({
+             "width": "100%"
+          })
+       });
+    }).blur(function() {
+       $(".spin").css({
+          "width": "0px"
+       })
+       if ($(this).val() == "") {
+          $(this).parent(".input").each(function() {
+             $("label", this).css({
+                "line-height": "60px",
+                "font-size": "24px",
+                "font-weight": "300",
+                "top": "10px"
+             })
+          });
+ 
+       }
+    });
+ 
+    $(".button").click(function(e) {
+       var pX = e.pageX,
+          pY = e.pageY,
+          oX = parseInt($(this).offset().left),
+          oY = parseInt($(this).offset().top);
+ 
+       $(this).append('<span class="click-efect x-' + oX + ' y-' + oY + '" style="margin-left:' + (pX - oX) + 'px;margin-top:' + (pY - oY) + 'px;"></span>')
+       $('.x-' + oX + '.y-' + oY + '').animate({
+          "width": "500px",
+          "height": "500px",
+          "top": "-250px",
+          "left": "-250px",
+ 
+       }, 600);
+       $("button", this).addClass('active');
     })
-  
-  
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
+ 
+    $(".alt-2").click(function() {
+       if (!$(this).hasClass('material-button')) {
+          $(".shape").css({
+             "width": "100%",
+             "height": "100%",
+             "transform": "rotate(0deg)"
+          })
+ 
+          setTimeout(function() {
+             $(".overbox").css({
+                "overflow": "initial"
+             })
+          }, 600)
+ 
+          $(this).animate({
+             "width": "140px",
+             "height": "140px"
+          }, 500, function() {
+             $(".box").removeClass("back");
+ 
+             $(this).removeClass('active')
+          });
+ 
+          $(".overbox .title").fadeOut(300);
+          $(".overbox .input").fadeOut(300);
+          $(".overbox .button").fadeOut(300);
+ 
+          $(".alt-2").addClass('material-buton');
+       }
+ 
+    })
+ 
+    $(".material-button").click(function() {
+ 
+       if ($(this).hasClass('material-button')) {
+          setTimeout(function() {
+             $(".overbox").css({
+                "overflow": "hidden"
+             })
+             $(".box").addClass("back");
+          }, 200)
+          $(this).addClass('active').animate({
+             "width": "700px",
+             "height": "700px"
+          });
+ 
+          setTimeout(function() {
+             $(".shape").css({
+                "width": "50%",
+                "height": "50%",
+                "transform": "rotate(45deg)"
+             })
+ 
+             $(".overbox .title").fadeIn(300);
+             $(".overbox .input").fadeIn(300);
+             $(".overbox .button").fadeIn(300);
+          }, 700)
+ 
+          $(this).removeClass('material-button');
+ 
+       }
+ 
+       if ($(".alt-2").hasClass('material-buton')) {
+          $(".alt-2").removeClass('material-buton');
+          $(".alt-2").addClass('material-button');
+       }
+ 
     });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    /*==================================================================
-    [ Show pass ]*/
-    var showPass = 0;
-    $('.btn-show-pass').on('click', function(){
-        if(showPass == 0) {
-            $(this).next('input').attr('type','text');
-            $(this).addClass('active');
-            showPass = 1;
-        }
-        else {
-            $(this).next('input').attr('type','password');
-            $(this).removeClass('active');
-            showPass = 0;
-        }
-        
-    });
-
-
-})(jQuery);
+ 
+ });
