48d3df6438c73550397cbec58552a729d5174d4b
Souravrax <rakshitsourav3@gmail.com>
2020-02-26 21:49:39 +0530

    Separated things

---
+++ assets/contests.js
@@ -0,0 +1,17 @@
+
+const data = [
+    {
+        "eventName": "Droidblitz",
+        "description": "ESPERANZA, in association with the CGEC Student  presents Sandrover for the participants of ESPERANZA CHAPTER 3.Droid Blitz is a double-elimination event that requires participants to build wireless-remote controlled electric-powered robots that can be used to complete broken pathways and transfer packages by running on land as well as water terrains. Build an amphibious robot that can be used to perform tasks on land and water and traverse across uneven terrains.It’s the time to wake up “guess” in you.​",
+        "videoURL": "https://youtube.com/randomURL",
+        "prizeMoney": 5000,
+        "rules": [1, 2, 3, 5, 6, 4]
+    },
+    {
+        "eventName": "Droidblitz",
+        "description": "ESPERANZA, in association with the CGEC Student  presents Sandrover for the participants of ESPERANZA CHAPTER 3.Droid Blitz is a double-elimination event that requires participants to build wireless-remote controlled electric-powered robots that can be used to complete broken pathways and transfer packages by running on land as well as water terrains. Build an amphibious robot that can be used to perform tasks on land and water and traverse across uneven terrains.It’s the time to wake up “guess” in you.​",
+        "videoURL": "https://youtube.com/randomURL",
+        "prizeMoney": 5000,
+        "rules": [1, 2, 3, 5, 6, 4]
+    }
+]
--- js/events.js
+++ js/events.js
@@ -9,6 +9,7 @@ const showPopUp = (e) => {
             contestDescription.innerHTML = data[0].description;
         }
         else{
+            contestName.innerHTML = "Something went wrong";
             console.log(`Sorry something went wrong`);
         }
     })
@@ -17,15 +18,3 @@ const showPopUp = (e) => {
 const hidePopUp = () => {
     popUp.classList.toggle("hide");
 }
-
-
-
-const data = [
-    {
-        "eventName": "Droidblitz",
-        "description": "ESPERANZA, in association with the CGEC Student  presents Sandrover for the participants of ESPERANZA CHAPTER 3.Droid Blitz is a double-elimination event that requires participants to build wireless-remote controlled electric-powered robots that can be used to complete broken pathways and transfer packages by running on land as well as water terrains. Build an amphibious robot that can be used to perform tasks on land and water and traverse across uneven terrains.It’s the time to wake up “guess” in you.​",
-        "videoURL": "https://youtube.com/randomURL",
-        "prizeMoney": 5000,
-        "rules": [1, 2, 3, 5, 6, 4]
-    }
-]
