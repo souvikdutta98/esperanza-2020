06f63c6f0e279f0eba07ed28a3f0a53c47dafcd9
Souravrax <rakshitsourav3@gmail.com>
2020-02-27 13:20:29 +0530

    Added content and finilized the functions

--- assets/contests.js
+++ assets/contests.js
@@ -2,7 +2,7 @@ const data = [
     {
         eventName: "Droidblitz",
         description: "ESPERANZA, in association with the CGEC Student  presents Sandrover for the participants of ESPERANZA CHAPTER 3.Droid Blitz is a double-elimination event that requires participants to build wireless-remote controlled electric-powered robots that can be used to complete broken pathways and transfer packages by running on land as well as water terrains. Build an amphibious robot that can be used to perform tasks on land and water and traverse across uneven terrains.It’s the time to wake up “guess” in you.​",
-        videoURL: "https://youtube.com/randomURL",
+        videoURL: "/videos/droidblitz.mp4",
         prizeMoney: 5000,
         rules: [
             "You have to made this bot Wirelessly.",
@@ -21,14 +21,130 @@ const data = [
             "The first round will be conducted in CGEC  during the fest. You have to made a bot which can cover some following steps. (the bot has to go through in plane path and then in  water)Selected students can participate in the 2nd Round.",
             "The bot has to complete the paths doing some given tasks."
         ],
-        contact : [
+        contact: [
             {
-                name : "person1",
-                no : 9999999999
+                name: "person1",
+                no: 9999999999
             },
             {
-                name : "person1",
-                no : 9999999999
+                name: "person1",
+                no: 9999999999
+            }
+        ]
+    },
+    {
+        eventName: "Robo Soccer",
+        description: "This is the turning point in the soccer game. In the spirit of the old fashioned board games and by taking a step forward, we have exchanged the tabletop game for the team of robot players connected with an app on a durable soccer pitch. The goal is no different from a regular soccer match: to attack, defend, strategize, and score more goals than your opponent.",
+        videoURL: "/videos/robosoccer.mp4",
+        prizeMoney: 5000,
+        rules: [
+            "You have to made the bot wirelessly/wired as your wish ...",
+            "We will provide you full training to make this type of bots ."
+        ],
+        registerURL: "https://register.for/this/contest",
+        contact: [
+            {
+                name: "Suman Sarkar(ECE 3rd year )",
+                no: 9064255724
+            },
+            {
+                name: "Pradip Paul(EE 3rd Year)",
+                no: 8017545276
+            }
+        ]
+    },
+    {
+        eventName: "Sand Rover",
+        description: "ESPERANZA, in association with the CGEC Student  presents Sandrover for the participants of ESPERANZA CHAPTER 3. Sand Rover is the ultimate designing challenge for you to showcase your  technical skills. It’s the time to wake up “guess” in you. ",
+        videoURL: "/videos/sandrover.mp4",
+        prizeMoney: 5000,
+        rules: [
+            "You have to make the bot Wireless.",
+            "Participants must be individual",
+            "Access to online sources of information is not permissible during the event.",
+            "Participants must be present for the event at the given time and given place.",
+            "All participants must report 15 minutes before the event starts.",
+            "Every participant should have their id card or at least one proof of identity.",
+            "All the participants are required to bring the registration receipt with them.",
+            "In case of any inconvenience or doubts during the competition, participants are required to seek the help of the volunteers present or to the event coordinator.",
+            "In case of any disrupts, the final world will remain with the organization.",
+            "The decision of the judges will be final."
+        ],
+        registerURL: "https://register.for/this/contest",
+        eventStructure: [
+            "The first round will be conducted in CGEC  during the fest. You have to made a bot which can cover some following steps. (the bot has to go through in sandy path, see saw path, inclined path, etc.)Selected students can participate in the 2nd Round.",
+            "The bot has to complete the paths carrying  with 150-200gm sands."
+        ],
+        contact: [
+            {
+                name: "Suman Sarkar(ECE 3rd year )",
+                no: 9064255724
+            },
+            {
+                name: "Pradip Paul(EE 3rd Year)",
+                no: 8017545276
+            }
+        ]
+    },
+    {
+        eventName: "Arm of Achelous",
+        description: "ESPERANZA, in association with the CGEC Student  presents Sandrover for the participants of ESPERANZA CHAPTER 3. Sand Rover is the ultimate designing challenge for you to showcase your  technical skills. It’s the time to wake up “guess” in you. ",
+        videoURL: "/videos/arm of achelous.mp4",
+        prizeMoney: 5000,
+        rules: [
+            "You have to make the bot Wireless.",
+            "Participants must be individual",
+            "Access to online sources of information is not permissible during the event.",
+            "Participants must be present for the event at the given time and given place.",
+            "All participants must report 15 minutes before the event starts.",
+            "Every participant should have their id card or at least one proof of identity.",
+            "All the participants are required to bring the registration receipt with them.",
+            "In case of any inconvenience or doubts during the competition, participants are required to seek the help of the volunteers present or to the event coordinator.",
+            "In case of any disrupts, the final world will remain with the organization.",
+            "The decision of the judges will be final."
+        ],
+        registerURL: "https://register.for/this/contest",
+        eventStructure: [
+            "The first round will be conducted in CGEC  during the fest. You have to made a bot which can cover some following steps. (the bot has to go through in sandy path, see saw path, inclined path, etc.)Selected students can participate in the 2nd Round.",
+            "The bot has to complete the paths carrying  with 150-200gm sands."
+        ],
+        contact: [
+            {
+                name: "Suman Sarkar(ECE 3rd year )",
+                no: 9064255724
+            },
+            {
+                name: "Pradip Paul(EE 3rd Year)",
+                no: 8017545276
+            }
+        ]
+    },
+    {
+        eventName: "Laws of Motion",
+        description: "ESPERANZA, in association with the CGEC Student  presents Laws of Motion for the participants of ESPERANZA CHAPTER 3. Laws 0f Motion is the ultimate designing challenge for you to showcase your  technical skills. It’s the time to wake up “guess” in you. ",
+        videoURL: "/videos/arm of achelous.mp4",
+        prizeMoney: "10,000/-",
+        rules: [
+            "You have to make the bot Wireless.",
+            "Participants must be individual",
+            "Access to online sources of information is not permissible during the event.",
+            "Participants must be present for the event at the given time and given place.",
+            "All participants must report 15 minutes before the event starts.",
+            "Every participant should have their id card or at least one proof of identity.",
+            "All the participants are required to bring the registration receipt with them.",
+            "In case of any inconvenience or doubts during the competition, participants are required to seek the help of the volunteers present or to the event coordinator.",
+            "In case of any disrupts, the final world will remain with the organization.",
+            "The decision of the judges will be final."
+        ],
+        registerURL: "https://register.for/this/contest",
+        eventStructure: [
+            "The first round will be conducted in CGEC  during the fest. You have to made a  Rc Plane which can Flight in sky till our given times.Selected students can participate in the 2nd Round.",
+            "The Rc Plane has to fly in sky doing some of our unique steps.(like: doing O shape in sky)"
+        ],
+        contact: [
+            {
+                name: "Mahitosh Gorai(CE 3rd year)",
+                no: 9134283811
             }
         ]
     }
--- js/events.js
+++ js/events.js
@@ -1,21 +1,122 @@
-const contestName = document.getElementById("contest-name");
 const popUp = document.querySelector("#pop-up");
+
+/*-----------------Pop Up configuration Starts-----------------*/
+
+const contestName = document.getElementById("contest-name");
+const contestBody = document.getElementById("contest-body-container");
 const contestDescription = document.getElementById("contest-description");
+const structure = document.getElementById("structure");
+const videoContainer = document.getElementById("video");
+const prizeMoney = document.getElementById("moneyPrize");
+const pm = document.getElementById("prize-money");
+const rules = document.getElementById("rules");
+const registerButton = document.querySelector(".register-button");
+
+/*-----------------Pop Up configuration Ends-----------------*/
+
+var toBeRemovedLaterElement = null;
+
 const showPopUp = (e) => {
-    popUp.classList.toggle("hide");
     data.forEach(elem => {
         console.log(elem.eventName);
         if (e.target.innerHTML == elem.eventName) {
-            contestName.innerHTML = elem.eventName;
-            contestDescription.innerHTML = elem.description;
-        }
-        else{
-            contestName.innerHTML = "Something went wrong";
-            console.log(`Sorry something went wrong`);
+            pm.classList.remove("hide");
+            contestBody.classList.remove("hide");
+            fillPopUp(elem);
+            toBeRemovedLaterElement = elem;
         }
     })
+    document.body.style.overflowY = "hidden";
+    popUp.classList.toggle("hide");
 }
 
 const hidePopUp = () => {
     popUp.classList.toggle("hide");
+    prizeMoney.removeChild(prizeMoney.firstChild);
+    contestName.removeChild(contestName.firstChild);
+    contestDescription.removeChild(contestDescription.firstChild);
+    structure.removeChild(structure.lastChild);
+    rules.removeChild(rules.lastChild);
+    if (toBeRemovedLaterElement.eventStructure != undefined) {
+        toBeRemovedLaterElement.eventStructure.forEach(elem => {
+            structure.removeChild(structure.lastChild);
+        })
+    }
+    toBeRemovedLaterElement.rules.forEach(elem => {
+        rules.removeChild(rules.lastChild);
+    })
+
+    /*################# Removing Video Element ################*/
+
+    let a = document.getElementById("video-player");
+    a.parentNode.removeChild(a);
+    structure.classList.remove("hide");
+
+    /*################# Hiding Unnecessary ################*/
+
+    pm.classList.add("hide");
+    contestBody.classList.add("hide");
+    document.body.style.overflowY = "scroll";
+
 }
+
+
+
+function fillPopUp(elem) {
+    contestName.appendChild(document.createTextNode(elem.eventName));
+    contestDescription.appendChild(document.createTextNode(elem.description));
+    prizeMoney.appendChild(document.createTextNode(elem.prizeMoney));
+
+
+    /*################# Adding Video Element ################*/
+
+    var video = document.createElement("video");
+    video.setAttribute("class", "video-js");
+    video.setAttribute("id", "video-player");
+    video.setAttribute("preload", "auto");
+    video.setAttribute("width", "50%");
+    video.setAttribute("height", "auto");
+    video.controls = true;
+    video.setAttribute("controlsList", "nodownload");
+    var source = document.createElement("source");
+    source.src = elem.videoURL;
+    source.type = "video/mp4";
+    source.setAttribute("id", "demo-video");
+    video.appendChild(source);
+    videoContainer.appendChild(video);
+
+
+
+    /*################# Forming the structure element ################*/
+
+    if (elem.eventStructure != undefined) {
+        var node = document.createElement('li');
+        var textNode = document.createTextNode(`Number of Rounds : ${elem.eventStructure.length}`);
+        node.appendChild(textNode);
+        node.style.fontWeight = 600;
+        node.style.listStyleType = "none";
+        node.style.color = "red";
+        structure.appendChild(node);
+        elem.eventStructure.forEach(element => {
+            var node = document.createElement('li');
+            var textNode = document.createTextNode(element);
+            node.appendChild(textNode);
+            structure.appendChild(node);
+        });
+        structure.classList.remove("hide");
+    }
+    else{
+        structure.classList.add("hide");
+    }
+
+    /*################# Forming the Rules and Regulation element ################*/
+
+    elem.rules.forEach(element => {
+        var node = document.createElement('li');
+        var textNode = document.createTextNode(element);
+        node.appendChild(textNode);
+        rules.appendChild(node);
+    });
+
+}
+
